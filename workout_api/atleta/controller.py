d23a2d7ac6ecb3a37cd192a2d824f87f25a5c9a7
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-07-08 21:34:56 -0300

    Inciando endpoint post atleta

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Body, status
 
 from workout_api.atleta.schemas import AtletaIn
 from workout_api.contrib.dependencies import DatabaseDependency
@@ -10,6 +10,9 @@ router = APIRouter()
     summary='Criar novo atleta',
     status_code=status.HTTP_201_CREATED
 )
-async def post(db_session: DatabaseDependency, atleta_in: AtletaIn):
+async def post(
+    db_session: DatabaseDependency, 
+    atleta_in: AtletaIn = Body()
+):
     pass
     
--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,14 +1,20 @@
 from typing import Annotated
-from pydantic import Field, PositiveFlot
+from pydantic import Field, PositiveFloat
 
-from workout_api.contrib.schemas import BaseSchema
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
-class AtletaIn(BaseSchema):
+class Atleta(BaseSchema):
     nome: Annotated[str, Field('Nome do Atleta', example='João', max_length=50)]
     cpf: Annotated[str, Field('CPF do Atleta', example='12345678900', max_length=11)]
     idade: Annotated[int, Field('Idade do Atleta', example=25)]
-    peso: Annotated[PositiveFlot, Field('Peso do Atleta', example=75.5)]
-    altura: Annotated[PositiveFlot, Field('Altura do Atleta', example=1.78)]
+    peso: Annotated[PositiveFloat, Field('Peso do Atleta', example=75.5)]
+    altura: Annotated[PositiveFloat, Field('Altura do Atleta', example=1.78)]
     sexo: Annotated[str, Field('Sexo do Atleta', example='M', max_length=1)]
+    
+class AtletaIn(Atleta):
+    pass
+    
+class AltetaOut(Atleta, OutMixin):
+    pass
     
 
